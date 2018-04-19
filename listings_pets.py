c307850fbbf29cf087b739958ee7afe1da079f67
chding12 <chding12@gmail.com>
2018-04-18 23:07:33 -0400

    update listings_pets

--- listings_pets.py
+++ listings_pets.py
@@ -6,17 +6,23 @@
 import pandas as pd
 
 
-# In[135]:
+# In[146]:
 
 listings = pd.read_csv('cleaned_data.csv')
+listings = listings[['id','neighbourhood_group_cleansed','amenities']]
 
 
-# In[136]:
+# In[147]:
 
 listings['amenities'] = listings['amenities'].apply(lambda x: x.replace('{','').replace('}','').replace('\"','').replace('/',' ').replace('’','').replace(',',' ').lower())
 
 
-# In[137]:
+# In[148]:
 
 listings.to_csv('listings_pets.csv')
+
+
+# In[ ]:
+
+
 
